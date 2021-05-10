88ca50399ab70456d1a1aebaa2aaf65d824ab6dc
RickyCervantesG <ricky.cervantes.guadalupe@gmail.com>
2021-05-10 17:00:49 -0400

    added my map

---
+++ maps.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = "pk.eyJ1Ijoic2xvd2Jybzg5MCIsImEiOiJja29peGYzYmUwczc5Mm5rMnNnaHI2ZDhvIn0.2WzdsUMONapZbsQ-m0hYLw"
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v10',
+  center: [-75.5244, 39.161079],
+  zoom: 8
+});
