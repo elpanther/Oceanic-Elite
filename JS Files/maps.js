5c08fe6323ce55ef96f854a0014eeda223e14ff5
RickyCervantesG <ricky.cervantes.guadalupe@gmail.com>
2021-05-18 16:58:26 -0400

    added data for a filter

--- JS Files/maps.js
+++ JS Files/maps.js
@@ -1,12 +1,12 @@
 
-
 mapboxgl.accessToken = "pk.eyJ1Ijoic2xvd2Jybzg5MCIsImEiOiJja29peGYzYmUwczc5Mm5rMnNnaHI2ZDhvIn0.2WzdsUMONapZbsQ-m0hYLw"
 
  var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11', // replace this with your style URL
     center: [-75.338675, 39.091951],
-    zoom: 7.7
+    zoom: 7.5
   });
+
 
 
